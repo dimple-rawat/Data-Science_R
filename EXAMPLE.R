8cf96e8589fb55924821bee709d00e3170613d0c
dimple-rawat <70286363+dimple-rawat@users.noreply.github.com>
2020-08-29 12:29:35 +0530

    Add files via upload

---
+++ EXAMPLE.R
@@ -0,0 +1,38 @@
+x <-10
+y<-20
+
+z<-100
+
+1+1  ADDITION
+
+x*y*z
+ 
+3 ^ 2  Exponentiation
+
+sqrt(10)
+
+pi    The CONSTANT PI IS PREDEFINED
+
+2*pi*6378
+
+#Create a vector
+
+X <- c(12,20,15,100)
+
+vect_x<-c(2,0,0,4)
+vect_y<- c(1,9,9,9)
+
+#add the vectors
+vect_add <- vect_x+vect_y
+
+#Mul vect_x with some scalar
+vect_x*5
+
+#Accessing the elements of the vector
+vect_x(1)
+vect_x(4)
+
+vect_x(2)<-2
+ 
+df <-data.frame(x=1:3,y=c("a","b","c"))
+df
